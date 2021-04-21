de27bf8fc1726e7f85fa4bc9c6365b67c1e32e5a
worud516 <worud516@naver.com>
2021-04-21 20:55:09 +0900

    study

--- C++Study/C++Study/Inheritance.cpp
+++ C++Study/C++Study/Inheritance.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 
 class Animal { //상속해주는 클래스의 멤버 변수, 함수를 외부세어도 활용할수있다.
-public:
+public: // public :아무나		protected : 외부x 내부o(자식은 내부임)		private:해당 클래스 안에서만 사용 가능		3개를 접근 지정자라고함
 	int legs;
 	char name[50];
 
@@ -29,14 +29,24 @@ public:
 		//legs = 2;
 		//strcpy(name, "사람");
 	}
-
 };
+
+class dog : public Animal {
+public:
+	dog() : Animal(4, "강아지") {
+		printf("dog 생성자 실행\n");
+	}
+};
+
 
 int main() { //정적인 클래스보다 동적인 클래스에서 더 많이 사용한다.
 
 	Person* p = new Person("1234-9999");
 
 	p->printfAnimalinfo();
-	
+
+	dog* d = new dog();
+	d->printfAnimalinfo();
+
 	return 0;
 }
